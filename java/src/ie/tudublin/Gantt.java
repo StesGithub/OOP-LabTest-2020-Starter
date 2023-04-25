8c3ae703b5be04158ce92cf75db20910985db4b8
StesGithub <123895888+StesGithub@users.noreply.github.com>
2023-04-25 15:26:56 +0100

    aweougbeaob

--- java/src/ie/tudublin/Gantt.java
+++ java/src/ie/tudublin/Gantt.java
@@ -1,6 +1,8 @@
 package ie.tudublin;
 
 import processing.core.PApplet;
+import java.util.Scanner;
+import java.io.*;
 
 public class Gantt extends PApplet
 {	
@@ -8,12 +10,25 @@ public class Gantt extends PApplet
 	public void settings()
 	{
 		size(800, 600);
+
+		try {
+			loadTasks();
+		} catch (FileNotFoundException e) {
+
+			e.printStackTrace();
+		}
+		printTasks();
 	}
 
-	public void loadTasks()
-	{
-		
-	}
+	public void loadTasks() throws FileNotFoundException{
+        Scanner sc = new Scanner(new File("/Users/stephen/Desktop/algosProject/OOP-LabTest-2020-Starter/java/data/tasks.csv"));
+        sc.useDelimiter(",");
+		while(sc.hasNext()){
+            System.out.println(sc.next());
+        }
+        sc.close();
+
+    }
 
 	public void printTasks()
 	{
@@ -34,10 +49,25 @@ public class Gantt extends PApplet
 	
 	public void setup() 
 	{
+		
 	}
 	
 	public void draw()
 	{			
-		background(0);
+
+
+		background(255,255,255);
+		stroke(0);
+		fill(0);
+		for(int i= 0; i<15; i++){
+
+			int space = 200;
+			int xSpacing = (space) + (40*i);
+
+			line(xSpacing, 50, xSpacing, 400);
+			textAlign(CENTER, CENTER);
+
+			text("X", xSpacing, 40);
+		}
 	}
 }
---
+++ java/src/ie/tudublin/Task.java
@@ -0,0 +1,56 @@
+package ie.tudublin;
+
+import java.util.Scanner;
+import java.io.*;
+
+import processing.data.TableRow;
+
+public class Task{
+    private String taskString;
+    private int taskX1;
+    private int taskX2;
+
+    
+    
+
+    public void task(String taskString, int taskX1, int taskX2){
+        this.taskString = taskString;
+        this.taskX1 = taskX1;
+        this.taskX2 = taskX2;
+        
+
+    }
+
+    public void TableRow(){
+       /// this.taskString = row.getString("taskString");
+        //this.taskX1 = row.getInt("taskX1");
+       // this.taskX2 = row.getInt("taskX2");
+    }
+
+
+
+    public String getString() {
+        return taskString;
+    }
+
+    public void setString(String newtaskString) {
+        this.taskString = newtaskString;
+    }
+
+    public int getTaskX1() {
+        return taskX1;
+    }
+
+    public void setTaskX1(int newtaskX1) {
+        this.taskX1 = newtaskX1;
+    }
+
+    public int getTaskX2() {
+        return taskX2;
+    }
+
+    public void setTaskX2(int newtaskX2) {
+        this.taskX2 = newtaskX2;
+    }
+
+}
